15c7ba62381145dd17af83cbd7d5e8ca86b5f0df
Anna Podlasek <a.m.podlasek@gmail.com>
2024-07-30 15:28:10 +0100

    Moving the methods to different class to separate it from View classes.

--- Haulage/Model/CustomerOrder.cs
+++ Haulage/Model/CustomerOrder.cs
@@ -1,10 +1,9 @@
-﻿
-
-using Haulage.Model.Constants;
+﻿using Haulage.Model.Constants;
 using Haulage.Model.Helpers;
 using SQLite;
 using System.ComponentModel.DataAnnotations.Schema;
 using ForeignKeyAttribute = SQLiteNetExtensions.Attributes.ForeignKeyAttribute;
+
 namespace Haulage.Model
 {
     public class CustomerOrder
@@ -16,17 +15,18 @@ namespace Haulage.Model
         public string Customer { get; set; }
 
         public Manifest Manifest { get { return this.manifest; } }
-        private Manifest manifest {  get; set; }
+        private Manifest manifest { get; set; }
 
         public Handover Handover { get { return this.handover; } }
         private Handover handover { get; set; } = null;
+
         [PrimaryKey]
         public Guid Id { get { return id; } set { this.id = value; } }
-        private Guid id  { get; set;}
+        private Guid id { get; set; }
+
         public Status Status { get { return status; } set { this.status = value; } }
 
         [ForeignKey(typeof(Warehouse))]
-
         public Guid WarehouseId { get; set; }
 
         private Status status { get; set; }
@@ -51,17 +51,27 @@ namespace Haulage.Model
 
         public void AddHandover(Handover handover)
         {
+            this.handover = handover;
+        }
 
-        this.handover = handover; }
-
-        public Handover ScheduleHandover(CustomerOrder order, DateTime handoverDate, bool pickup)
+        public Handover ScheduleHandover(DateTime handoverDate, bool pickup)
         {
-            this.handover = new Handover(order.Id, handoverDate, pickup);
+            this.handover = new Handover(this.Id, handoverDate, pickup);
             this.status = pickup ? Status.AWAITING_PICKUP : Status.EXPECTED;
             DB.connection.Update(this);
-            return this.Handover;
+            return this.handover;
+        }
+
+        public void ConfirmPickup()
+        {
+            this.status = Status.EXPECTED;
+            DB.connection.Update(this);
+        }
+
+        public void ConfirmDelivery()
+        {
+            this.status = Status.COLLECTED;
+            DB.connection.Update(this);
         }
     }
-
-
 }
--- Haulage/View/OrderPreview.xaml.cs
+++ Haulage/View/OrderPreview.xaml.cs
@@ -3,131 +3,108 @@ using Haulage.Model;
 using Haulage.Model.Users;
 using Microsoft.Maui.Controls;
 
-namespace Haulage.View;
-
-public partial class OrderPreview : ContentPage
+namespace Haulage.View
 {
-    private CustomerOrder order;
-    private Customer customer;
-    private CustomerController controller;
-	public OrderPreview(CustomerOrder orderToPreview, Customer customer)
-	{
-        InitializeComponent();
-        this.customer = customer;
-        this.controller = new CustomerController(customer.Login);
-        UserName.Text = "Currently logged in as " + customer.Login;
-        OrderId.Text = "Currently viewing Order " + orderToPreview.Id;
-        order = orderToPreview;
-        Items.ItemsSource = orderToPreview.Manifest.Items;
-        Total.Text = "Total for this order is " + orderToPreview.Manifest.Total;
-
-        if(orderToPreview.Handover != null)
-        {
-            string pickup = String.Format("Your {0} is scheduled for {1}", "pickup", orderToPreview.Handover.ExpectedHandover);
-            string deliver = String.Format("Your {0} is scheduled for {1}", "delivery", orderToPreview.Handover.ExpectedHandover);
-            HandoverDate.Text = orderToPreview.Handover.Pickup ? pickup : deliver;
-        }
-
-        // Set button visibility based on order status
-        if (orderToPreview.Status == Model.Constants.Status.AWAITING_PICKUP)
-        {
-            ConfirmPickup.IsVisible = true;
-        }
-        else
-        {
-            ConfirmPickup.IsVisible = false;
-        }
-
-        Status.Text = "Current order status " + orderToPreview.Status;
-        if(orderToPreview.Status == Model.Constants.Status.PENDING)
-        {
-            PickupDate.IsVisible = true;
-            DatePicker.IsVisible = true;
-            DatePicker.IsEnabled = true;
-            TimePicker.IsEnabled = true;
-            TimePicker.IsVisible = true;
-            PickupBtn.IsEnabled = true;
-            PickupBtn.IsVisible = true;
-        }
-
-        // Show "Confirm Delivery" button only if status is EXPECTED
-        if (orderToPreview.Status == Model.Constants.Status.EXPECTED)
-        {
-            ConfirmDelivery.IsVisible = true;
-        }
-        else
-        {
-            ConfirmDelivery.IsVisible = false;
-        }
-    }
-
-    private async void ConfirmPickup_Clicked(object sender, EventArgs e)
+    public partial class OrderPreview : ContentPage
     {
-        try
+        private CustomerOrder order;
+        private Customer customer;
+        private CustomerController controller;
+
+        public OrderPreview(CustomerOrder orderToPreview, Customer customer)
         {
-            // Change the status to EXPECTED
-            order.Status = Model.Constants.Status.EXPECTED;
-            // Update the order in the database
-            DB.connection.Update(order);
+            InitializeComponent();
+            this.customer = customer;
+            this.controller = new CustomerController(customer.Login);
+            UserName.Text = "Currently logged in as " + customer.Login;
+            OrderId.Text = "Currently viewing Order " + orderToPreview.Id;
+            order = orderToPreview;
+            Items.ItemsSource = orderToPreview.Manifest.Items;
+            Total.Text = "Total for this order is " + orderToPreview.Manifest.Total;
 
-            await DisplayAlert("Success", "Pickup confirmed. Status updated to EXPECTED.", "OK");
+            if (orderToPreview.Handover != null)
+            {
+                string pickup = String.Format("Your {0} is scheduled for {1}", "pickup", orderToPreview.Handover.ExpectedHandover);
+                string deliver = String.Format("Your {0} is scheduled for {1}", "delivery", orderToPreview.Handover.ExpectedHandover);
+                HandoverDate.Text = orderToPreview.Handover.Pickup ? pickup : deliver;
+            }
 
-            // Refresh the page
-            App.Current.MainPage = new NavigationPage(new OrderPreview(order, customer));
+            Status.Text = "Current order status " + orderToPreview.Status;
+
+            // Set button visibility based on order status
+            ConfirmPickup.IsVisible = (orderToPreview.Status == Model.Constants.Status.AWAITING_PICKUP);
+            ConfirmDelivery.IsVisible = (orderToPreview.Status == Model.Constants.Status.EXPECTED);
+
+            if (orderToPreview.Status == Model.Constants.Status.PENDING)
+            {
+                PickupDate.IsVisible = true;
+                DatePicker.IsVisible = true;
+                DatePicker.IsEnabled = true;
+                TimePicker.IsEnabled = true;
+                TimePicker.IsVisible = true;
+                PickupBtn.IsEnabled = true;
+                PickupBtn.IsVisible = true;
+            }
         }
-        catch (Exception ex)
-        {
-            ErrorMessage.Text = ex.Message;
-        }
-    }
 
-    private async void ConfirmDelivery_Clicked(object sender, EventArgs e)
-    {
-        try
-        {
-            // Change the status to COLLECTED
-            order.Status = Model.Constants.Status.COLLECTED;
-            // Update the order in the database
-            DB.connection.Update(order);
-
-            await DisplayAlert("Success", "Delivery confirmed. Status updated to COLLECTED.", "OK");
-
-            // Refresh the page
-            App.Current.MainPage = new NavigationPage(new OrderPreview(order, customer));
-        }
-        catch (Exception ex)
-        {
-            ErrorMessage.Text = ex.Message;
-        }
-    }
-
-
-
-    private async void PickupBtn_Clicked(object sender, EventArgs e)
-    {
-        if (DatePicker.Date > DateTime.Today || (DatePicker.Date == DateTime.Today && TimePicker.Time >= DateTime.Now.TimeOfDay))
+        private async void ConfirmPickup_Clicked(object sender, EventArgs e)
         {
             try
             {
-                Handover handover = controller.ScheduleHandover(order, DatePicker.Date+TimePicker.Time, true);
-                await DisplayAlert("Pickup scheduled", handover.ExpectedHandover.ToString(),"Accept");
-                order.AddHandover(handover);
-                App.Current.MainPage = new NavigationPage(new OrderPreview(order,customer));
+                order.ConfirmPickup();
+                await DisplayAlert("Success", "Pickup confirmed. Status updated to EXPECTED.", "OK");
+                RefreshPage();
             }
             catch (Exception ex)
             {
                 ErrorMessage.Text = ex.Message;
             }
         }
-        else
-        {
-            ErrorMessage.Text = "Please select a valid date to pickup";
-        }
-    }
 
-    private async void BackToCustomerPageBtn_Clicked(object sender, EventArgs e)
-    {
-        // Navigate back to the customer page
-        App.Current.MainPage = new NavigationPage(new CustomerPage(customer)); // Assuming CustomerPage is the page for customer overview
+        private async void ConfirmDelivery_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                order.ConfirmDelivery();
+                await DisplayAlert("Success", "Delivery confirmed. Status updated to COLLECTED.", "OK");
+                RefreshPage();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage.Text = ex.Message;
+            }
+        }
+
+        private async void PickupBtn_Clicked(object sender, EventArgs e)
+        {
+            if (DatePicker.Date > DateTime.Today || (DatePicker.Date == DateTime.Today && TimePicker.Time >= DateTime.Now.TimeOfDay))
+            {
+                try
+                {
+                    Handover handover = controller.ScheduleHandover(order, DatePicker.Date + TimePicker.Time, true);
+                    await DisplayAlert("Pickup scheduled", handover.ExpectedHandover.ToString(), "Accept");
+                    order.AddHandover(handover);
+                    RefreshPage();
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage.Text = ex.Message;
+                }
+            }
+            else
+            {
+                ErrorMessage.Text = "Please select a valid date to pickup";
+            }
+        }
+
+        private void RefreshPage()
+        {
+            App.Current.MainPage = new NavigationPage(new OrderPreview(order, customer));
+        }
+
+        private async void BackToCustomerPageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new CustomerPage(customer));
+        }
     }
 }
